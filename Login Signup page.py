716a6a8b46b966cdd10b1313a55a34428777d0ec
sheetalmalviya900 <91360201+sheetalmalviya900@users.noreply.github.com>
2022-08-08 16:18:55 +0530

    Update Login Signup page.py

--- Login Signup page.py
+++ Login Signup page.py
@@ -1,78 +1,68 @@
 import json
-
-print("WELCOME TO LOGIN AND SIGNUP PAGE")
-
-def signup():
-    mydata=open("userdetail.json","r")
-    r = mydata.read()
-    my = json.loads(r)
-    for i in my:
-        name=input("Enter the name : ")
-        if name in my[i]["Name"]:
-            print("Username is already exist in file")
-            print(" TRY AGAIN ")
-            break
-        phone=int(input("Enter the number : "))
-        pas=input("Enter the password whos first letter is in upper case,contain only 6 character,special character and number :" )
-        if len(pas)==6:
-            pass;
-            if ("@" in pas) or ("#" in pas) or ("$" in pas) :
-                pass;
-                if '1' in pas or  '2' in pas or '3' in pas or '4' in pas or '5' in pas or '6' in pas or '7' in pas or '8' in pas or '9'in pas or '0' in pas :
-                    pass;
-                    if pas[0]>="A" and pas[0]<="Z":
-                        print("STRONG PASSWORD")
-                        confirmpswrd=input("Enter password one more time for confiormation ")
-                        if confirmpswrd==pas:
-                            print("YOUR ACCOUNT IS SUCESSFULLY CREATED")
-                            dob=input("Enter the date of birth : ")
-                            gender=(input("Enter the Gender : "))
+import os
+import re
+print("      WELCOME IN LOGIN AND SIGNUP PAGE!      ")
+file=os.path.exists("signup.json")
+if file ==False:
+        l=[]
+        with open("signup.json","a") as f:
+            a=json.dump(l,f,indent=2)
+            print(a)
+            
+user=input("what you want to do login or signup  : ")
+if user=="signup":
+        n=input("Enter your name : ")
+        n1=input("enter your lastname : ")
+        r=open("signup.json","r")
+        n2=r.read()
+        if n not in n2:
+            print("create a strong password with a mixture of letter number and symbols")
+            def pas():
+                p=input("Enter your passward : ")
+                if (re.search("[a-z A-Z]",p) and re.search("[0-9]",p) and (re.search("[@#$&]",p))):
+                    if len(p)>=8:
+                        confirm_p=input("Enter the confirm password : ")
+                        if (p==confirm_p):
+                            print("coorect password")     
+                            dob=input("Enter your date of birth")
+                            g=input("Enter your gender male or female : ")
+                            gmail=input("enter your own gmail : ")    
+                            print(n,"your signup is *succesfully*")
+                            dic={n:{"Name":n,"Password":p,"DOB":dob,"Gender":g,"gmail":gmail}}
+                            with open("signup.json","r") as f:
+                                data=json.load(f)
+                            data.append(dic)
+                            with open("signup.json","w") as f:
+                                json.dump(data,f,indent=2)
                         else:
-                            print("password is not same")
+                            print("password not mached") 
+                            pas()
+                            
                     else:
-                        print("Atleast add one upper case")
+                        print("password must be longer than8 charater") 
+                        pas()
+                        
                 else:
-                    print("add one number")
-            else:
-                print("special character miss in password")
+                    print("invalid password","password must be mixture of letter number and symbols")
+            pas()
         else:
-            print("enter the password length is 6")
-        
-        my[name]={"Name":name,"Number":phone,"password":pas,"Dob":dob,"Gender":gender}
-    # for i in my:
-        # if name in my[i]["Name"]:
-        #     print("Username is already exist in file")
-        #     print(" TRY AGAIN ")
-        #     break
-        with open("userdetail.json","w") as mydata:
-            json.dump(my,mydata,indent=4)
+           print("Name is already exist")
 
-def login():
-
-    mydata=open("userdetail.json","r")
-    r = mydata.read()
-    my = json.loads(r)
-
-    username=input(" Enter the name ")
-    password=input("Enter the password")
-    for i  in my:
-        if username in my[i]["Name"]:
-            if password in my[i]["password"]:
-                print(" YOUR PAGE IS SUCESSFULLY LOGIN")
-                break
-            else:
-                print("your password is wrong ")
-                print("Try again")
-        else:
-            print("This username name not find in file")
-            print("first signup your page")
-
-def begin():
-    option=input("what you want login or signup :")
-    if option=="signup":
-        signup()
-    elif option=="login":
-        login()
-    else:
-        print("Lets start with begin ")
-begin()
+elif user=="login":
+        user_name=input("Enter your name :")
+        u1=input("Enter your passward:")
+        with open("signup.json","r") as f:
+            da=json.load(f)
+            for i in range(len(da)): 
+                if da[i]["Name"]==user_name:
+                    if da[i]["Password"]==u1:
+                        print("*login successfully*")
+                        print("your name is",da[i]["Name"],"\n")
+                        print("and your data is :-\n")
+                        for x,y in da[i].items():
+                            print(x,'=',y)                          
+                    else:
+                        print("incorrect password")
+                        break
+                else:
+                    print("Name is not exist")
